f7a46a12401f9b0ea4c2bdb4a6bba5a8496c5ccf
pablo-fdz <pablo.fdzalb@gmail.com>
2025-06-12 09:40:18 +0200

    Modified parquet by shuffling data within splits

--- library/preprocessing/__init__.py
+++ library/preprocessing/__init__.py
@@ -1,5 +1,3 @@
 from .lower_nostop import lower_nostop
 from .lemmatize import lemmatize
 from .stem import stem
-from .word2vec_mean_embedding import word2vec_mean_embedding
-from .word2vec_tfidf_weighted_embedding import word2vec_tfidf_weighted_embedding
